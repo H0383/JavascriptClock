32544318676f2bd08f1b7352fe3314e2926fd4b2
H0383 <69017905+H0383@users.noreply.github.com>
2020-08-02 16:58:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+// console.log("Hello World")
+// console.log("This is Sam");
+
+// var num1 = 56;
+// var num2 = 34;
+// console.log(num1 + num2);
+
+// var marks = {
+//     Sam: 56,
+//     deepu: 34,
+//     Avi: 67
+// }
+// console.log(marks);
+
+let mydate = new Date();
+console.log(mydate);
